725fdf0a96768867b5d6a6c6d3f504956f801fef
Adrian Wydra <wydra.adrian98@gmail.com>
2020-07-21 06:59:01 +0200

    structure change

--- src/main/scala/NoTransactionNewVersion/SourceProducent.scala
+++ src/main/scala/NoTransactionNewVersion/SourceProducent.scala
@@ -1,43 +1,35 @@
 package NoTransactionNewVersion
 
-import NoTransaction.ProducerUtil.{randomAmount, randomPrice, takeProductName}
-import NoTransaction.{Product, UniqueId}
-import akka.kafka.ProducerMessage
-import akka.kafka.scaladsl.Producer
+import akka.kafka.{ProducerMessage, ProducerSettings}
 import akka.stream.scaladsl.{Sink, Source}
 import akka.stream.{ActorMaterializer, Materializer}
-import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.{ProducerConfig, ProducerRecord}
+import org.apache.kafka.common.serialization.StringSerializer
+
 import scala.concurrent.duration._
 
 object SourceProducent extends App {
   implicit val system = akka.actor.ActorSystem("system")
   implicit val materializer: Materializer = ActorMaterializer()
 
-  var i = 0
+  val config = system.settings.config.getConfig("akka.kafka.producer")
+  val producerSettings =
+    ProducerSettings(config, new StringSerializer, new StringSerializer)
+      .withBootstrapServers("localhost:9092")
+      .withProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+      .withProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+
+  var number = 0
   val done = Source
     .tick(1.second, 1.second, "")
     .map { _ =>
-      if (i % 10 == 0 & i > 0) {
-        println()
-        UniqueId.updateId()
-        i = 0
+      if (number % 10 == 0 & number > 0) {
+        number = 0
       }
-      i = i + 1
+      number = number + 1
       ProducerMessage.single(
-        new ProducerRecord[String, String]("topic1",
-          new Product(UniqueId.getId(), takeProductName(), randomAmount(), randomPrice()).toString)
+        new ProducerRecord[String, Int]("topicToSource", number)
       )
     }
-    .via(Producer.flexiFlow(Properties.producerSettings))
-    .map {
-      case ProducerMessage.Result(metadata, ProducerMessage.Message(record, passThrough)) => {
-        s"Wysyłam $i produkt:${record.value().split(",")(1)} " +
-          s"ilość:${record.value().split(",")(2)} " +
-          s"cena za jeden ${record.value().split(",")(3).toDouble} " +
-          s"paragonu o id ${UniqueId.getId()}"
-      }
-    }
     .runWith(Sink.foreach(println(_)))
-
-
 }
